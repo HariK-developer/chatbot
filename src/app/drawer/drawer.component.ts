571bef4c516d166f6604deddeefeefabbcd9a633
HariK-developer <hariswdeveloper@gmail.com>
2024-08-25 16:53:13 +0530

    chat style completed

--- src/app/drawer/drawer.component.ts
+++ src/app/drawer/drawer.component.ts
@@ -1,25 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-
-
+import { HomeComponent } from '../home/home.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'drawer',
   standalone: true,
   templateUrl: './drawer.component.html',
   styleUrls: ['./drawer.component.css'],
-  imports: [ CommonModule ]
- 
+  imports: [CommonModule],
 })
 export class DrawerComponent {
   @Input() today: boolean | undefined;
+  @Input() homeComponent!: HomeComponent;
   isDrawerOpen = true;
-
+  constructor(private router: Router) { }
   toggleDrawer() {
     this.isDrawerOpen = !this.isDrawerOpen;
   }
 
   closeDrawer() {
     this.isDrawerOpen = false;
+  }
+
+  reloadCurrentRoute() {
+    this.router.navigate([this.router.url]);
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { DrawerComponent } from '../drawer/drawer.component';
 import { CommonModule } from '@angular/common';
 import { GeminiService } from '../service/gemini.service';
@@ -16,11 +16,13 @@ export class HomeComponent {
   enable = false;
   response_text: string = '';
 
+
   constructor(private geminiService: GeminiService) {}
 
   async sendMessage() {
     this.showHome = false;
-    // this.response_text = await this.geminiService.run(this.text);
+    this.response_text = await this.geminiService.run(this.text);
+    // this.response_text = "In the depths of the Pacific Ocean, lived a Great White Shark named Mako. He wasn't like the other sharks, though. He could understand humans and felt a powerful urge to protect them. When a massive oil spill threatened the coast, Mako, with his powerful tail and razor-sharp teeth, became the unlikely hero, rallying the marine life and using his strength to push the oil away from the shore. From then on, Mako, the  became a legend, a protector of the seas, proving that even the most feared creatures can become the greatest heroes."
   }
 
   typing(event: Event) {
@@ -31,5 +33,11 @@ export class HomeComponent {
       this.enable = false;
     }
     this.text = target.value;
+  }
+
+  home () {
+    this.showHome = true;
+    console.log("I am here");
+    
   }
 }
